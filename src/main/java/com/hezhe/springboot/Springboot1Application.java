a32506a089b111c0afc52c94aa9937a512ee80f4
贺哲 <hezhe1688@163.com>
2020-01-11 11:35:15 +0800

    定时任务

--- src/main/java/com/hezhe/springboot/Springboot1Application.java
+++ src/main/java/com/hezhe/springboot/Springboot1Application.java
@@ -3,7 +3,9 @@ package com.hezhe.springboot;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling //开启定时任务
 @MapperScan(basePackages = "com.hezhe.springboot.mapper")
 @SpringBootApplication
 public class Springboot1Application {
